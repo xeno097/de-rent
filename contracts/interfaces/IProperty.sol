65f6c6265855c04cb6e3e9f048f104b2f22baf56
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-01-26 23:41:06 -0400

    feat(Listing): implemented and tested the Listing contract

--- contracts/interfaces/IProperty.sol
+++ contracts/interfaces/IProperty.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
-interface IProperty is IERC721 {
+interface IProperty is IERC721Metadata {
     /**
      * @dev Returns the total number of minted properties.
      */
