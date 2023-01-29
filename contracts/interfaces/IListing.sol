36857231ecd30d1c84f97da7193356ab66a24e2a
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-01-29 19:06:23 -0400

    feat: added the IListingFacet interface

--- contracts/interfaces/IListing.sol
+++ contracts/interfaces/IListing.sol
@@ -23,3 +23,29 @@ interface IListing {
      */
     function getPropertyById(uint256 property) external view returns (DataTypes.ListingProperty memory);
 }
+
+interface IListingFacet {
+    /**
+     * @dev Returns the properties owned by sender.
+     */
+    function getSelfProperties() external view returns (DataTypes.ListingProperty[] memory);
+
+    /**
+     * @dev Returns the properties that have the `published` field set to true.
+     */
+    function getPublishedProperties() external view returns (DataTypes.ListingProperty[] memory);
+
+    /**
+     * @dev Returns all the properties existing on the platform.
+     */
+    function getProperties() external view returns (DataTypes.ListingProperty[] memory);
+
+    /**
+     * @dev Returns `property` if exists.
+     *
+     * Requirements:
+     * - `property` must exist.
+     *
+     */
+    function getPropertyById(uint256 property) external view returns (DataTypes.ListingProperty memory);
+}
