a7ded400a712b6ba40de6f85a66caf38e4b0a2e3
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-01-31 14:11:45 -0400

    feat: implemented the IPropertyFacet interface

--- contracts/interfaces/IProperty.sol
+++ contracts/interfaces/IProperty.sol
@@ -32,3 +32,34 @@ interface IProperty is IERC721Metadata {
      */
     function updateMetadata(uint256 property, string memory uri) external;
 }
+
+interface IPropertyFacet {
+    /**
+     * @dev Returns the total number of minted properties.
+     */
+    function getTotalPropertyCount() external view returns (uint256);
+
+    /**
+     * @dev Allows sender to update the `property` visibility.
+     *
+     * Requirements:
+     * - sender must be the `property` owner
+     * - `property` must exist.
+     *
+     */
+    function setPropertyVisibility(uint256 property, bool visibility) external;
+
+    /**
+     * @dev Allows sender to mint a new property with the given metadata uri.
+     */
+    function createProperty(string memory uri, uint256 rentPrice) external;
+
+    /**
+     * @dev Allows sender to update the `property` uri metadata.
+     * Requirements:
+     * - sender must be the `property` owner
+     * - `property` must exist.
+     *
+     */
+    function updateProperty(uint256 property, string memory newUri) external;
+}
