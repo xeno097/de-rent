6d98ec5f04a5301058df2e74cab75f056e0b0aaa
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-01-31 20:08:16 -0400

    refactor: removed the balances mapping from the AppStorage struct

--- contracts/libraries/AppStorage.sol
+++ contracts/libraries/AppStorage.sol
@@ -10,7 +10,6 @@ struct AppStorage {
     // Core
     mapping(uint256 => DataTypes.Property) properties;
     mapping(uint256 => DataTypes.Rental) rentals;
-    mapping(address => uint256) balances;
     // Reputation
     mapping(address => ScoreCounters.ScoreCounter) userScores;
     mapping(address => ScoreCounters.ScoreCounter) userPaymentPerformanceScores;
--- test/utils/BaseTest.sol
+++ test/utils/BaseTest.sol
@@ -41,31 +41,31 @@ abstract contract BaseTest is Test {
     }
 
     function _setPropertyCount(address target, uint256 value) internal {
-        bytes32 sslot = bytes32(uint256(10));
+        bytes32 sslot = bytes32(uint256(9));
 
         _writeToStorage(target, sslot, bytes32(value));
     }
 
     function _createTokenBalance(address target, uint256 tokenId, address owner, uint256 balance) internal {
-        bytes32 sslot = keccak256(abi.encode(uint256(uint160(owner)), keccak256(abi.encode(tokenId, uint256(6)))));
+        bytes32 sslot = keccak256(abi.encode(uint256(uint160(owner)), keccak256(abi.encode(tokenId, uint256(5)))));
 
         _writeToStorage(target, sslot, bytes32(balance));
     }
 
     function _createTokenUri(address target, uint256 tokenId, string memory uri) internal {
-        bytes32 sslot = keccak256(abi.encode(tokenId, uint256(9)));
+        bytes32 sslot = keccak256(abi.encode(tokenId, uint256(8)));
 
         _writeToStorage(target, sslot, bytes32(bytes(uri)));
     }
 
     function _setTokenOwner(address target, uint256 tokenId, address owner) internal {
-        bytes32 sslot = keccak256(abi.encode(tokenId, uint256(11)));
+        bytes32 sslot = keccak256(abi.encode(tokenId, uint256(10)));
 
         _writeToStorage(target, sslot, bytes32(uint256(uint160(owner))));
     }
 
     function _getUserScoreStorageSlot(address user) internal pure returns (bytes32) {
-        return keccak256(abi.encode(uint256(uint160(user)), uint256(3)));
+        return keccak256(abi.encode(uint256(uint160(user)), uint256(2)));
     }
 
     function _createUserScore(address target, address user, uint256 totalCount, uint256 voteCount) internal {
@@ -84,7 +84,7 @@ abstract contract BaseTest is Test {
     }
 
     function _getUserPaymentPerformanceScoreStorageSlot(address user) internal pure returns (bytes32) {
-        return keccak256(abi.encode(uint256(uint160(user)), uint256(4)));
+        return keccak256(abi.encode(uint256(uint160(user)), uint256(3)));
     }
 
     function _createUserPaymentPerformanceScore(address target, address user, uint256 totalCount, uint256 voteCount)
@@ -109,7 +109,7 @@ abstract contract BaseTest is Test {
     }
 
     function _getPropertyScoreStorageSlot(uint256 property) internal pure returns (bytes32) {
-        return keccak256(abi.encode(property, uint256(5)));
+        return keccak256(abi.encode(property, uint256(4)));
     }
 
     function _createPropertyScore(address target, uint256 property, uint256 totalCount, uint256 voteCount) internal {
